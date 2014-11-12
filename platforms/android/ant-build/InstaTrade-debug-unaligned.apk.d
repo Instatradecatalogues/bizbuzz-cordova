6dfc3976ede698d71fd4f94c52ce2c6a62364c23
Sushil <p12sushilkm@iimahd.ernet.in>
2014-11-12 17:56:22 +0530

    After creating first launch application

---
+++ platforms/android/ant-build/InstaTrade-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/home/sushil/workspace/bizbuzz/cordova/platforms/android/ant-build/InstaTrade-debug-unaligned.apk : \
+/home/sushil/workspace/bizbuzz/cordova/platforms/android/ant-build/InstaTrade.ap_ \
+/home/sushil/workspace/bizbuzz/cordova/platforms/android/ant-build/classes.dex \
+/home/sushil/workspace/bizbuzz/cordova/platforms/android/CordovaLib/ant-build/classes.jar \
---
+++ platforms/android/gen/com/bizbuzz/instatrade/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.bizbuzz.instatrade;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
